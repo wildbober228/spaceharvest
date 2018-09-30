25f8e9b83e69117619629be72711df0ac38a2bae
wildbober228 <irina20000@tut.by>
2018-09-30 16:59:59 +0300

    new version 1.1 for danik

---
+++ Assets/ArcadeSpaceFlight/Code/station/random_station.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class random_station : auto_station {
+    auto_station m = new auto_station();
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
+
+
